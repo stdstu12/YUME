2e27bd0edb90f4110874fe3c0ea044f27fdb1898
stdstu12 <xfmao23@m.fudan.edu.cn>
2025-07-15 12:47:43 +0800

    Delete run_sample.sh

--- run_sample.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-srun -J job --partition Gveval-T --gres gpu:4 --nodes 1 --ntasks-per-node 1 --cpus-per-task 26 --quotatype reserved -o slurm_output/slurm-%j.out -e slurm_output/slurm-%j.err scripts/inference/sample_image.sh
