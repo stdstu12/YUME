440a1b90c190311d88c571eecaaf28f062a3e13a
stdstu12 <xfmao23@m.fudan.edu.cn>
2025-07-24 00:40:35 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-srun -J job --partition eaigc1 --gres gpu:8 --nodes 1 --ntasks-per-node 1 --cpus-per-task 128 --quotatype reserved -o slurm_output/slurm-%j.out -e slurm_output/slurm-%j.err scripts/distill/distill_hunyuan_model.sh
