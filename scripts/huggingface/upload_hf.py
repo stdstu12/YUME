578f3108559047ce74bda1f762d384cdc7bfb6d9
stdstu12 <xfmao23@m.fudan.edu.cn>
2025-07-24 08:23:49 +0800

    Delete scripts/huggingface directory

--- scripts/huggingface/upload_hf.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-
-api.upload_folder(
-    folder_path="data/Black-Myth-Taylor-Src",
-    repo_id="FastVideo/Image-Vid-Finetune-Src",
-    repo_type="dataset",
-)
